69f79b73b24faecc96e696fcc9c1587f4fabdc95
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-19 14:08:57 +0100

    structuration

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<title></title>
+<head>
+    <ol>
+        <li>Classification</li>
+        <li>Thesaurus</li>
+        <li>Typologie</li>
+        <li>Delai conservation</li>
+        <li>Référentiel</li>
+    </ol>
+</head>
+</html>
+
