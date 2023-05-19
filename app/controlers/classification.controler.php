6ae5b5845592adb72e0e541286826c44e816fab6
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-19 14:50:48 +0100

    ajout des controllers

---
+++ app/controlers/classification.controler.php
@@ -0,0 +1,10 @@
+<?php
+if($_GET['q'] == "classification" && isset($_GET['section'])){
+    switch($_GET['section']){
+        case 'classe': ;
+        break;
+        case 'search': ;
+        break;
+    }
+}
+?>
---
+++ app/controlers/home.controler.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET['q'])){
+    switch($_GET['q']){
+        case 'classification': include 'app/controlers/classification.controler.php';
+        break; 
+        case 'communicability': include 'app/controlers/communicability.controler.php';
+        break;
+        case 'retention': include 'app/controlers/retention.controler.php';
+        break;
+        case 'thesaurus': include 'app/controllers/thesaurus.controler.php';
+        break;
+        case 'typology': include 'app/controlers/typology.controler.php';
+        break;
+        case 'reference': include 'app/controlers/reference.controler.php';
+        break;
+        case 'setting': include 'app/controlers/setting.controler.php';
+        break;
+        default : include 'app/controlers/classification.controler.php';
+    }
+}
+?>
