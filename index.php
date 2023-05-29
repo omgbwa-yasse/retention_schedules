ac6a92f87156fbdacd4b8bd235b465e8815d8b25
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-30 00:03:13 +0100

    Structure de la connexion effectuée

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
 <?php
-include_once 'app/controllers/home.controller.php';
 
-if (session_status() !== PHP_SESSION_ACTIVE) {
-  session_start();
-}
+include 'app/controllers/home.controller.php';
 
-if (isset($_SESSION['username'])) {
-  header('Location: index.php?q=home');
-  exit();
-}
+?>   
 
-if (isset($_POST['login'])) {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  if ($username == 'admin' && $password == 'secret'){
-    $_SESSION['username'] = $username;
-    header('Location: index.php?q=home');
-    exit();
-  } else {
-    echo 'Invalid username or password';
-  }
-}
-?>
-<html>
-<head>
-  <title>Login</title>
-</head>
-<body>
-  <h1>Login</h1>
-  <form method="post" action="index.php">
-    <p>Username: <input type="text" name="username"></p>
-    <p>Password: <input type="password" name="password"></p>
-    <p><input type="submit" name="login" value="Login"></p>
-  </form>
-</body>
-</html>
