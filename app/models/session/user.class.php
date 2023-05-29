93ecda1b71dec7f6bf885ee1cf3ba7d2df29fe25
omgbwa yasse <omgbwayasse@gmail.com>
2023-05-30 00:19:45 +0100

    modified:   app/models/session/user.class.php

--- app/models/session/user.class.php
+++ app/models/session/user.class.php
@@ -2,7 +2,7 @@
 require_once 'app/models/connexion.class.php';
 
 class user extends connexion{
-    private $_user_pseudo;
+    private $_user_login;
     private $_user_password;
     private $_user_id;
     private $_user_name;
@@ -11,7 +11,7 @@ class user extends connexion{
     private $_user_birthday;
 
     public function __construct(){
-        $this->_user_pseudo;
+        $this->_user_login;
         $this->_user_password;
         $this->_user_name;
         $this->_user_surname;
@@ -38,9 +38,9 @@ class user extends connexion{
         $this->_user_password = sha1(sha1($password).$this->getUserSand());
     }
  
-    public function setUserSandByPseudo() {
-        $stmt = $this->getCnx()->prepare('SELECT user_sand FROM user WHERE user_pseudo = :pseudo');
-        $stmt->execute([':pseudo' => $this->getUserPseudo()]);
+    public function setUserSandBylogin() {
+        $stmt = $this->getCnx()->prepare('SELECT user_sand FROM user WHERE user_login = :login');
+        $stmt->execute([':login' => $this->getUserlogin()]);
         if ($stmt->rowCount() > 0) {
             $row = $stmt->fetch();
             $this->setUserSand($row['user_sand']);
@@ -53,17 +53,17 @@ class user extends connexion{
 
     public function setUserId($id) { $this->_user_id = $id; }
 
-    public function getUserPseudo() { return $this->_user_pseudo; }
+    public function getUserlogin() { return $this->_user_login; }
 
-    public function setUserPseudo($Pseudo) {  $this->_user_pseudo = $Pseudo; }
+    public function setUserlogin($login) {  $this->_user_login = $login; }
 
     public function getUserPassword() {  return $this->_user_password;}
 
     public function setUserPassword($password) { $this->_user_password = $password; }
 
 public function passwordVerification() {
-    $stmt = $this->getCnx()->prepare('SELECT user_password FROM user WHERE user_pseudo = :user_pseudo');
-    $stmt->execute([':user_pseudo' => $this->getUserPseudo()]);
+    $stmt = $this->getCnx()->prepare('SELECT user_password FROM user WHERE user_login = :user_login');
+    $stmt->execute([':user_login' => $this->getUserlogin()]);
     if ($stmt->rowCount() > 0) {
         $row = $stmt->fetch();
         if($this->getUserPassword() == $row['user_password']){ return true; } else { return false; };
@@ -74,8 +74,8 @@ public function passwordVerification() {
 
 
     public function userExists() {
-        $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_pseudo = :user_pseudo');
-        $stmt->execute([':user_pseudo' => $this->getUserPseudo()]);
+        $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_login = :user_login');
+        $stmt->execute([':user_login' => $this->getUserlogin()]);
         if ($stmt->rowCount() > 0) {
             return true;
         } else {
@@ -85,14 +85,14 @@ public function passwordVerification() {
     
     public function connect(){
         if($this->userExists() && $this->passwordVerification()){
-            $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_pseudo = :user_pseudo AND user_password = :user_password');
-            $stmt->execute([':user_pseudo' => $this->getUserPseudo(), ':user_password' => $this->getUserPassword()]);
+            $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_login = :user_login AND user_password = :user_password');
+            $stmt->execute([':user_login' => $this->getUserlogin(), ':user_password' => $this->getUserPassword()]);
             if ($stmt->rowCount() > 0) {
                 foreach ($stmt as $user) {
                     $this->setUserId($user['user_id']);
                     $this->setUserName($user['user_name']);
                     $this->setUserSurname($user['user_surname']);
-                    $this->setUserPseudo($user['user_pseudo']);
+                    $this->setUserlogin($user['user_login']);
                     $this->setUserPassword($user['user_password']);
                     $this->setUserSand($user['user_sand']);
                     $this->setUserBirthday($user['user_birthday']);
@@ -104,19 +104,19 @@ public function passwordVerification() {
         }
     }
 
-    public function changePassword($user_pseudo, $newPassword) {
-        $stmt = $this->getCnx()->prepare('UPDATE user SET user_password = :newPassword WHERE user_pseudo = :user_pseudo');
-        $stmt->execute([':newPassword' => $newPassword, ':user_pseudo' => $user_pseudo]);
+    public function changePassword($user_login, $newPassword) {
+        $stmt = $this->getCnx()->prepare('UPDATE user SET user_password = :newPassword WHERE user_login = :user_login');
+        $stmt->execute([':newPassword' => $newPassword, ':user_login' => $user_login]);
     }
 
-    public function changePseudo($user_pseudo, $newPseudo) {
-        $stmt = $this->getCnx()->prepare('UPDATE user SET user_pseudo = :newPseudo WHERE user_pseudo = :user_pseudo');
-        $stmt->execute([':newPseudo' => $newPseudo, ':user_pseudo' => $user_pseudo]);
+    public function changelogin($user_login, $newlogin) {
+        $stmt = $this->getCnx()->prepare('UPDATE user SET user_login = :newlogin WHERE user_login = :user_login');
+        $stmt->execute([':newlogin' => $newlogin, ':user_login' => $user_login]);
     }
     public function saveUser(){
-        $stmt = $this->getCnx()->prepare("INSERT INTO user(user_pseudo,user_name,user_surname,user_password, user_sand, user_birthday) VALUES(:user_pseudo,:user_name,:user_surname,:user_password,:user_sand,:user_birthday)");
+        $stmt = $this->getCnx()->prepare("INSERT INTO user(user_login,user_name,user_surname,user_password, user_sand, user_birthday) VALUES(:user_login,:user_name,:user_surname,:user_password,:user_sand,:user_birthday)");
         $stmt->execute([
-            ':user_pseudo' => $this->getUserPseudo(),
+            ':user_login' => $this->getUserlogin(),
             ':user_name' => $this->getUserName(),
             ':user_surname' => $this->getUserSurname(),
             ':user_password' => $this->getUserPassword(),
@@ -125,12 +125,12 @@ public function passwordVerification() {
         ]);
         return $stmt->rowCount() > 0;
     }
-    public function deleteUser($user_pseudo, $user_password) {
-        $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_pseudo = :user_pseudo AND user_password = :user_password');
-        $stmt->execute([':user_pseudo' => $user_pseudo, ':user_password' => $user_password]);
+    public function deleteUser($user_login, $user_password) {
+        $stmt = $this->getCnx()->prepare('SELECT * FROM user WHERE user_login = :user_login AND user_password = :user_password');
+        $stmt->execute([':user_login' => $user_login, ':user_password' => $user_password]);
         if ($stmt->rowCount() > 0) {
-            $stmt = $this->getCnx()->prepare('DELETE FROM user WHERE user_pseudo = :user_pseudo AND user_password = :user_password');
-            $stmt->execute([':user_pseudo' => $user_pseudo, ':user_password' => $user_password]);
+            $stmt = $this->getCnx()->prepare('DELETE FROM user WHERE user_login = :user_login AND user_password = :user_password');
+            $stmt->execute([':user_login' => $user_login, ':user_password' => $user_password]);
             return true;
         } else {
             return false;
